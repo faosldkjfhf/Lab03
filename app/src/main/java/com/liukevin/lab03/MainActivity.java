d0792c359319a42dd4e2723955a98f3454ea670f
Kevin Liu <dethsandwich@gmail.com>
2020-02-14 13:09:25 -0500

    Fri Feb 14

--- app/src/main/java/com/liukevin/lab03/MainActivity.java
+++ app/src/main/java/com/liukevin/lab03/MainActivity.java
@@ -2,7 +2,11 @@ package com.liukevin.lab03;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SharedPreferences sharedPreferences = getSharedPreferences("Settings", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        TextView bottomLeft;
+        TextView bottomRight;
+        TextView topRight;
+        TextView topLeft;
+        bottomLeft = findViewById(R.id.bottomLeft);
+        topLeft = findViewById(R.id.topLeft);
+        topRight = findViewById(R.id.topRight);
+        bottomRight = findViewById(R.id.bottomRight);
+        View.OnClickListener onClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        };
+
     }
 }
