53f5a52a98fe894264b1eb39d057768742a3e5ab
Kevin Liu <dethsandwich@gmail.com>
2020-02-14 13:48:20 -0500

    Fri Feb 14

--- app/src/main/java/com/liukevin/lab03/MainActivity.java
+++ app/src/main/java/com/liukevin/lab03/MainActivity.java
@@ -5,31 +5,45 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView bottomLeft;
+    TextView bottomRight;
+    TextView topRight;
+    TextView topLeft;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SharedPreferences sharedPreferences = getSharedPreferences("Settings", Context.MODE_PRIVATE);
+        final SharedPreferences sharedPreferences = getSharedPreferences("Settings", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
-        TextView bottomLeft;
-        TextView bottomRight;
-        TextView topRight;
-        TextView topLeft;
         bottomLeft = findViewById(R.id.bottomLeft);
         topLeft = findViewById(R.id.topLeft);
         topRight = findViewById(R.id.topRight);
         bottomRight = findViewById(R.id.bottomRight);
-        View.OnClickListener onClickListener = new View.OnClickListener() {
+        View.OnClickListener click = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                TextView temp = (TextView)v;
+                int settings = sharedPreferences.getInt(temp.getText().toString(), 0);
+                editor.putInt(temp.getText().toString(), settings + 1);
+                Log.i("buttonPressed", String.valueOf(settings + 1) + " " + temp.getText().toString());
+                editor.apply();
 
+                Context context = getApplicationContext();
+                Toast toast = Toast.makeText(context, temp.getText().toString() + ": " + String.valueOf(settings + 1), Toast.LENGTH_SHORT);
+                toast.show();
             }
         };
+        bottomLeft.setOnClickListener(click);
+        bottomRight.setOnClickListener(click);
+        topLeft.setOnClickListener(click);
+        topRight.setOnClickListener(click);
 
     }
 }
